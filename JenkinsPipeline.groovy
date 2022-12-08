3413a7f9d577180a03d9fd1a783f8c4330b809bb
MarinaPysarenko <oreshonkova23@gmail.com>
2022-12-08 14:47:28 +0200

    tert

--- JenkinsPipeline.groovy
+++ JenkinsPipeline.groovy
@@ -1,37 +1,37 @@
-pipeline{
-agent any
-options { timestamps() }
-stages {
-    stage("Tools initialization") {
-        steps {
-            sh "mvn --version"
-            sh "java -version"
+pipeline {
+    agent any
+    options { timestamps() }
+    stages {
+        stage("Tools initialization") {
+            steps {
+                sh "mvn --version"
+                sh "java -version"
+            }
         }
-    }
-    stage('git') {
-        steps {
-            git credentialsId: 'dda4af50-a518-422c-99e3-59c8d822940f', url: 'https://github.com/MarinaPysarenko/MentoringCourse.git'
-            sh 'git pull origin master'
+        stage('git') {
+            steps {
+                git credentialsId: 'dda4af50-a518-422c-99e3-59c8d822940f', url: 'https://github.com/MarinaPysarenko/MentoringCourse.git'
+                sh 'git pull origin master'
+            }
         }
-    }
 
-    stage('Test FramesPage') {
-        steps {
-            sh 'mvn test -Dtest=FramesPageTest'
+        stage('Test FramesPage') {
+            steps {
+                sh 'mvn test -Dtest=FramesPageTest'
+            }
+        }
+        stage('Test ModalsTab') {
+            steps {
+                sh "mvn test -Dtest=ModalsTabTest#openSmallModal"
+            }
+        }
+        post('Allure report') {
+            always {
+                steps {
+                    allure includeProperties: false, jdk: '', results: [[path: 'allure-results']]
+                }
+            }
         }
     }
-    stage('Test ModalsTab') {
-        steps {
-            sh "mvn test -Dtest=ModalsTabTest#openSmallModal"
-        }
-    }
-}
-    post('Allure report') {
-        always{
-        steps {
-            allure includeProperties: false, jdk: '', results: [[path: 'allure-results']]
-        }
-    }
-}
 }
 
